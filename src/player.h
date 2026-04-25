39b926f17e14031abec52e33843c72a1c5314661
Skickle <skicklel@gmail.com>
2026-04-25 19:50:14 -0300

    Criado efeito de particulas para o modelo da nave

--- src/player.h
+++ src/player.h
@@ -4,13 +4,24 @@
 #include "raylib.h"
 #include "globals.h"
 
+#define MAX_PARTICULAS 60
+
+typedef struct {
+    Vector3 posicao;
+    Vector3 velocidade;
+    float tamanho;
+    float vida;
+    bool ativa;
+}Particula;
+
 typedef struct {
     Vector3 posicao;
     Model modelo;
+    Particula particulas[MAX_PARTICULAS];
 }Nave;
 
 void inicializarNave(Nave *jogador);
-void atualizarNave(Nave *jogador);
+void atualizarNave(Nave *jogador, float deltaTime);
 void desenharNave(Nave *jogador);
 void descarregarNave(Nave *jogador);
 
